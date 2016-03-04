bced736fa6d3cf718b4c223456ebf728cc051647
Des <f01030606@student.ait.ie>
2016-03-04 18:05:40 +0000

    Added a "Add Meal" activity.

--- app/src/main/java/com/example/des/project/MainActivity.java
+++ app/src/main/java/com/example/des/project/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.des.project;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -9,15 +14,59 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final int TAKE_PIC_REQUEST_CODE = 0;
+    public static final int CHOOSE_PIC_REQUEST_CODE = 1;
+    public static final int MEDIA_TYPE_IMAGE = 2;
+
+    protected Button mAddImageBtn;
+    protected Button mUploadImageBtn;
+    protected ImageView mPreviewImageView;
+
+    private Uri mMediaUri;
+
+    public void queryImagesFromParse(){
+        ParseQuery<ParseObject> imagesQuery = new ParseQuery<>("ImageUploads");
+        imagesQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> images, ParseException e) {
+                if(e == null){
+
+                    MainActivityAdapter adapter = new MainActivityAdapter(MainActivity.this, images);
+                   // setListAdapter(adapter);
+
+                }else{
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +75,99 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        queryImagesFromParse();
+
+        //initialize
+        mAddImageBtn = (Button)findViewById(R.id.addImageButton);
+        mUploadImageBtn = (Button)findViewById(R.id.uploadImageButton);
+        mPreviewImageView = (ImageView)findViewById(R.id.previewImageView);
+
+        //listen to add button click
+        mAddImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //show dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Upload or Take a photo");
+                builder.setPositiveButton("Upload", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //upload image
+                        Intent choosePictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        choosePictureIntent.setType("image/*");
+                        startActivityForResult(choosePictureIntent, CHOOSE_PIC_REQUEST_CODE);
+
+                    }
+                });
+                builder.setNegativeButton("Take Photo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //take photo
+                        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                        if (mMediaUri == null) {
+                            //display error
+                            Toast.makeText(getApplicationContext(), "Sorry there was an error! Try again.", Toast.LENGTH_LONG).show();
+                        } else {
+                            takePicture.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                            startActivityForResult(takePicture, TAKE_PIC_REQUEST_CODE);
+                        }
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        //listen to upload button click
+        mUploadImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create parse object for image to upload
+                final ParseObject imageUpload = new ParseObject("ImageUploads");
+                try {
+                    //convert image to bytes for upload.
+                    byte[] fileBytes = FileHelper.getByteArrayFromFile(MainActivity.this, mMediaUri);
+                    if (fileBytes == null) {
+                        //there was an error
+                        Toast.makeText(getApplicationContext(), "There was an error. Try again!", Toast.LENGTH_LONG).show();
+                    } else {
+
+                        fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+                        String fileName = FileHelper.getFileName(MainActivity.this, mMediaUri, "image");
+                        final ParseFile file = new ParseFile(fileName, fileBytes);
+                        imageUpload.saveEventually(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+
+                                    imageUpload.put("imageContent", file);
+                                    imageUpload.saveInBackground(new SaveCallback() {
+                                        @Override
+                                        public void done(ParseException e) {
+                                            Toast.makeText(getApplicationContext(), "Success Uploading iMage!", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                                } else {
+                                    //there was an error
+                                    //there was an error
+                                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    }
+
+                } catch (Exception e1) {
+                    Toast.makeText(getApplicationContext(), e1.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+
 
        /* final TextView textViewToChange = (TextView) findViewById(R.id.user_test);
         textViewToChange.setText(currentUser);*/
@@ -44,7 +186,85 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+
+
     }
+
+    //inner helper method
+    private Uri getOutputMediaFileUri(int mediaTypeImage) {
+
+        if(isExternalStorageAvailable()){
+            //get the URI
+            //get external storage dir
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "UPLOADIMAGES");
+            //create subdirectore if it does not exist
+            if(!mediaStorageDir.exists()){
+                //create dir
+                if(! mediaStorageDir.mkdirs()){
+
+                    return null;
+                }
+            }
+            //create a file name
+            //create file
+            File mediaFile = null;
+            Date now = new Date();
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+
+            String path = mediaStorageDir.getPath() + File.separator;
+            if(mediaTypeImage == MEDIA_TYPE_IMAGE){
+                mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+            }
+            //return file uri
+            Log.d("UPLOADIMAGE", "FILE: " + Uri.fromFile(mediaFile));
+
+            return Uri.fromFile(mediaFile);
+        }else {
+
+            return null;
+        }
+
+    }
+    //check if external storage is mounted. helper method
+    private boolean isExternalStorageAvailable(){
+        String state = Environment.getExternalStorageState();
+        if(state.equals(Environment.MEDIA_MOUNTED)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == CHOOSE_PIC_REQUEST_CODE){
+                if(data == null){
+                    Toast.makeText(getApplicationContext(), "Image cannot be null!", Toast.LENGTH_LONG).show();
+                }else{
+                    mMediaUri = data.getData();
+                    //set previews
+                    mPreviewImageView.setImageURI(mMediaUri);
+                }
+            }else {
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(mMediaUri);
+                sendBroadcast(mediaScanIntent);
+                //set previews
+
+                mPreviewImageView.setImageURI(mMediaUri);
+
+            }
+
+        }else if(resultCode != RESULT_CANCELED){
+            Toast.makeText(getApplicationContext(), "Cancelled!", Toast.LENGTH_LONG).show();
+        }
+    }*/
+
 
     @Override
     public void onBackPressed() {
@@ -105,9 +325,53 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // The below is the method used when a click is registered from the "AddMeal" floating action button.
+    public void clickFABAddMeal(View view) {
+        Intent i = new Intent(MainActivity.this, AddMealActivity.class);
+        startActivity(i);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     // The below is the method used when a click is registered from the floating action buttons
     public void clickFAB(View view) {
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
             startActivity(intent);
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void clickFABscan(View view) {
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        startActivity(intent);
+    }
+
+
+    public void scanQRCode(View v) {
+        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+        integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result =
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            String contents = result.getContents();
+            if (contents != null) {
+             //   String scanRsult =
+                //showDialog(R.string.result_succeeded, result.toString());
+            } else {
+               // showDialog(R.string.result_failed,
+               //         getString(R.string.result_failed_why));
+            }
+        }
+    }
+
+    private void showDialog(int title, CharSequence message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+       // builder.setPositiveButton(R.string.ok_button, null);
+        builder.show();
+    }
+
 }
